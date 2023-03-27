d98463247c679ab75a73bc41094060533a8079e1
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-03-27 17:33:27 +0000

    More pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This a header file containing declarations
+ * for prototype functions defined in the 0x05-pointers_arrays_strings
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ */
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char _strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
