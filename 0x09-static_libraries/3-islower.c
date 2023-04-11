b785a039fddae5d8199c457a08bf6bfeac26c811
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-04-11 04:42:56 +0000

    3-islower.c static library

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ *@c: the characters mention
+ * Return: 1 if c is lowercase 0 therwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
