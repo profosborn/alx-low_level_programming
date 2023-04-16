746213c6a97a9e1c43c9cefa52080f0d7bf349f1
Edudzi Worlasi Osborn <oeworlasi@gmail.com>
2023-04-15 21:13:12 -0700

    Script updated

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c *.c
+gcc -Wall -pedantic -Werror -Wextra -c 0-islower.c 0-isdigit.c 1-ismemcpy.c \
+    1-strncat.c 100-atoi.c 2-strchr.c 2-strlen.c 2-strncpy.c 3-islower.c \
+    3-puts.c 3-strcmp.c 3-strspn.c 4-isalpha.c 4-strpbrk.c 5-strstr.c \
+    6-abs.c 9-strcpy.c
 ar -rcs liball.a *.o
 ranlib liball.a
