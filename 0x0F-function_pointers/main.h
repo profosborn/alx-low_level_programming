302a1821b7b1b3a876585749f3f028c7abe7e6e3
Edudzi Worlasi Osborn <oeworlasi@gmail.com>
2023-04-19 05:33:17 -0700

    Main header

---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* This is a header file containing declarations
+ * for prototype functions defined in the 0x07-more-functions_nested_loops
+ * tasks and directory
+ * This is like creating my own library with declared functions
+ * The tasks defines  the functions
+ *
+ *
+ * enclosing the files  contentinbetween ifndef,define and endif,
+ * stops the compiler from processing the file twice if its included twice,
+ * if not so, this will cause an error
+ * */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+int (*get_op_func(char *s))(int, int);
+
+
+#endif
