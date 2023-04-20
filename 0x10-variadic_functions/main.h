50cca4123af656a1b9b52ee9d9838100649d2d54
EDUDZI WORLASI OSBORN <oeworlasi@gmail.com>
2023-04-20 09:54:17 +0000

    main.h header file

---
+++ 0x10-variadic_functions/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print_all(const char * const format, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
