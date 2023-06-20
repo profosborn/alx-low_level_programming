42458da9d2316dbb8ad9540f9176f3d794fc2fa1
Edudzi Worlasi Osborn <oeworlasi@gmail.com>
2023-06-19 20:52:17 -0700

    Task 0 completed

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @t: integer
+ * Return: Always 0 positive
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
